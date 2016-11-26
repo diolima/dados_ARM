0063f1e87b51b2515164eb30fed3878ea21d37af
Lucas Cardozo <lucasecardozo@gmail.com>
2016-11-26 17:43:49 +0000

    november plots

--- visualization/exploratory.R
+++ visualization/exploratory.R
@@ -5,17 +5,18 @@ library(ggthemes)
 fulldata <- fread("../data/allvariables.tsv") # Fast read data.table
 setnames(fulldata, tolower(colnames(fulldata))) # Lowercase column names
 
-# Date Formatting
+## Data Formatting ##
 fulldata[, day_hour := as.POSIXct(strptime(paste(day, hour, sep=" "), "%Y-%m-%d %H"))] 
 fulldata[, day := as.Date(day)]
 fulldata[, month := as.numeric(format.Date(day, "%m"))]
+fulldata[, year := as.numeric(format.Date(day, "%Y"))]
 
-# data.table reshaping (wide to long)
-m.fulldata <- melt(fulldata[], id=c('day_hour', 'day', 'hour', 'month')) # Melts wide data.table
+m.fulldata <- melt(fulldata[], id=c('day_hour', 'day', 'hour', 'month', 'year')) # Melts wide data.table
 m.fulldata[, c("measured_var", "stats") := tstrsplit(variable, "_", fixed=T)]
 m.fulldata[is.infinite(value), value := NA]
 
-# Exploratory plots by day and month
+
+## Exploratory plots by day and month ##
 granularity <- c('day', 'month')
 sapply(granularity, function(gran){
 	pdf(paste0(gran, '_summary.pdf'), h=10, w=10)
@@ -31,7 +32,22 @@ sapply(granularity, function(gran){
 	print(g)
 	dev.off()
 })
-# December and November appear to be the months with less outliers and less NA values #
+# November appears to be the month with less outliers and less NA values #
+
+sapply(granularity[1], function(gran){
+	pdf(paste0(gran, '_nov_summary.pdf'), h=10, w=10)
+	g <- ggplot(m.fulldata[stats=='mean' & month==11], aes(get(gran), value, group=variable)) +
+				stat_summary(fun.ymax=max, fun.ymin=min, geom='ribbon', aes(fill=variable)) +
+				stat_summary(fun.y=mean, geom='line', color='black') + theme_few() +
+				theme_few() + 
+				facet_grid(variable~year, scales="free") + xlab(gran) 
+	if(gran == 'day'){
+		g <- g + scale_x_date(date_breaks='1 month', date_labels = "%b %d") +
+			     theme(axis.text.x=element_text(angle=45, hjust=1))
+	}
+	print(g)
+	dev.off()
+})
 
 
 # Variables distribuition
@@ -55,6 +71,13 @@ g <- ggplot(m.fulldata[stats=="mean"], aes(x=variable, y=value, fill=as.factor(m
 							  outlier.color='black', outlier.alpha=0.5, position=position_dodge(1)) +
 						#scale_fill_manual(values=c('steelblue3', 'springgreen2'), labels = c('Nov', 'Dec'), name='Months') +
 						facet_wrap(~variable, scales="free") + theme_few() + theme(axis.text.x=element_blank())
+print(g)
+dev.off()
+
+pdf('correlations.pdf', h=10, w=10)
+g <- ggpairs(fulldata[, grep('mean', colnames(fulldata)), with=F],
+		lower=list(continuous=wrap('points', size=0.4, alpha=0.3))) + 
+		theme_few()
 print(g)
 dev.off()
 
