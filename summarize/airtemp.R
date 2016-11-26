07ddb3bb30b21e90607b47f0e8b6fb0d3e9b2ecf
diogenes_lima <s.lima.diogenes@gmail.com>
2016-11-26 03:23:06 -0200

    fixed airtemp.R

--- summarize/airtemp.R
+++ summarize/airtemp.R
@@ -1,10 +1,12 @@
-# Todos os arquivos devem estar no mesmo local. Neste caso os arquivos que 
+# All files must be in the same folder. 
 library(data.table)
 library(parallel)
 
-files <- grep('.tsv', dir(), value = T)
+files <- grep('.tsv', 
+	      dir('~/work/raw_proj/wind_data/springston-met', full.names = T),
+	      value = T)
 
-reduz_gran <- function(arquivo){
+summarize <- function(arquivo){
    nov <- fread(arquivo, skip = 39, data.table = F)
    colnames(nov) <- c('datetime', 'wspeed', 'wdir', 'airtemp', 'rh', 'baro', 'rain')
    # Get dates and character to use as factor to aggregate
@@ -16,7 +18,7 @@ reduz_gran <- function(arquivo){
    hour <- gsub(".* (\\d{2}).*", '\\1', nov$datetime)
    m_d_h <- paste(year, month, day, hour, sep = '_')
    print(sample(m_d_h, 10))
-   # Mantain only useful variables
+   # Maintain only useful variables
    dat <- nov[,c('wspeed', 'wdir', 'airtemp', 'rh', 'baro', 'rain')]
    aggdata_mean <- aggregate(dat, by = list(m_d_h), FUN = mean, na.rm = T)
    aggdata_min <- aggregate(dat, by = list(m_d_h), FUN = min, na.rm = T)
@@ -31,12 +33,34 @@ reduz_gran <- function(arquivo){
    output
 }
 
-output <- mclapply(files, reduz_gran, mc.cores = 12)
+# Parallel lapply to read and summarize data
+output <- mclapply(files, summarize, mc.cores = 12)
 
-# Pegar o mês do nome dos arquivos.
+# Retrieving month from file names.
 names(output) <- substr(sapply(strsplit(files, '\\.'), '[', 5), 1, 6)
+output <- output[order(names(output))]
 
-soma_de_nas <- sapply(output, function(x){
+# Getting NA sum for each month
+na_sum <- sapply(output, function(x){
   sum(is.na(x))
 })
 
+# Appending full data frame
+out_df <- do.call(rbind, output)
+out_df$day <- as.Date(out_df$day, format = '%Y_%m_%d')
+rownames(out_df) <- NULL
+
+
+# Manual test 
+#    mock_df <- out_df[complete.cases(out_df),]
+#    set.seed(42)
+#    mock_set <- mock_df[sample(1:nrow(mock_df), 3),]
+#    full_df <- fread(files[22], skip = 39, data.table = F)
+#    colnames(full_df) <- c('datetime', 'wspeed', 'wdir', 'airtemp', 'rh', 'baro', 'rain')
+#    only_selected <- subset(full_df, grepl('^2015-10-03 04:', full_df$datetime))
+#    summary(only_selected)
+### wdir_mean == 82.42
+### Everything is correct. Saving file and moving on ###
+
+write.table(out_df, './../data/vaisala.tsv', quote = F, 
+	    sep = '\t', row.names = F)
